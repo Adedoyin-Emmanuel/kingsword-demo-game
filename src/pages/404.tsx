a6ea957183769748f87e73e9cccad824317ae3db
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-17 00:29:13 +0100

    Updated the 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NotFound = (): JSX.Element =>{
+    
+    return (
+        <React.Fragment>
+            
+            <section className="container-fluid">
+            
+            </section>
+            
+        </React.Fragment>
+    )
+
+}
+
+
+export default NotFound;
