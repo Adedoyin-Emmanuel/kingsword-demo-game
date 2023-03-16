ba76c947b4858b94c81f0de5c3be76a1f664209f
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-17 00:31:50 +0100

    Updated the home component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = (): JSX.Element => {
+  return (
+    <React.Fragment>
+      <section className="container-fluid">
+        <h1>welcome home!</h1>
+      </section>
+    </React.Fragment>
+  );
+};
+
+
+export default Home;
