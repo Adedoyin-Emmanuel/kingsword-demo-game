510cd8b4f081ad6175c6d66ed1d76c1f81266a57
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-17 00:32:57 +0100

    Updated the result page

---
+++ src/pages/Result.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Result = (): JSX.Element => {
+  return (
+    <React.Fragment>
+      <section className="container-fluid">
+        <h1>you failed!</h1>
+      </section>
+    </React.Fragment>
+  );
+};
+
+
+export default Result;
