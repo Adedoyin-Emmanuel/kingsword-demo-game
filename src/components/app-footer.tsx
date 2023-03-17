e6e3a6fc7e7d8bd9214584fbc3efa3650b0f03f4
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-17 02:13:26 +0100

    Added the footer navs and icons to the footer component

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -6,8 +6,18 @@ import RecentIcon from "./../assets/images/recent.svg";
 const Footer = ():JSX.Element => {
     return (
         <React.Fragment>
-            <section className="app-footer">
+            <section className="app-footer d-flex align-items-center justify-content-around">
+                <section className="home">
+                    <img src={HomeIcon} alt="Home" height={"20"} width={"20"}/>
+                </section>
                 
+                <section className="home">
+                    <img src={CategoryIcon} alt="Category" height={"20"} width={"20"}/>
+                </section>
+                
+                <section className="home">
+                    <img src={RecentIcon} alt="Recent" height={"20"} width={"20"}/>
+                </section>
             </section>
         </React.Fragment>
     )
