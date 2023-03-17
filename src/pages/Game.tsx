a797c26abe624cdbf4be16419ca4a0382d8b48c6
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-17 10:46:09 +0100

    Added the footer, current page footer notch

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { useLocation, useParams } from 'react-router-dom';
-
+import { useParams } from 'react-router-dom';
+import Footer from '../components/app-footer';
+import CurrentPageNotch 
+from '../components/app-current-page-notch';
 const Game =  ():JSX.Element => {
 
     const {category} = useParams();
     
     return(
         <React.Fragment>
-            <section className="container-fluid">
-               <h3 className="fs-3 fw-bold"> You selected the {category} category</h3>
+            <section className="container-fluid p-0">
+               <h3 className="fs-3 fw-bold my-3 p-2"> You selected the {category} category</h3>
+               <Footer categoryPage={<CurrentPageNotch/>}/>
             </section>
         </React.Fragment>
     );
