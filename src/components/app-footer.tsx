212b93704fb823a51ed116a96cf49cddfa6da444
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-17 03:12:52 +0100

    Added the current-page-notch component

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -2,24 +2,51 @@ import React from "react";
 import CategoryIcon from "./../assets/images/category.svg";
 import HomeIcon from "./../assets/images/home.svg";
 import RecentIcon from "./../assets/images/recent.svg";
-import CurrentPageNotch from "./app-current-page-notch";
-const Footer = (): JSX.Element => {
+
+interface footerActivePageNotch {
+  homePage?: JSX.Element;
+  categoryPage?: JSX.Element;
+  recentPage?: JSX.Element;
+}
+const Footer = ({
+  homePage,
+  categoryPage,
+  recentPage,
+}: footerActivePageNotch): JSX.Element => {
   return (
     <React.Fragment>
-      <section className="app-footer d-flex align-items-center justify-content-around brand-category-love-color p-2">
-        <section className="home d-flex align-items-center justify-content-center flex-column">
-          <img src={HomeIcon} alt="Home" height={"18"} width={"18"}/>
-          <CurrentPageNotch/>
+      <section className="app-footer d-flex align-items-center justify-content-between brand-category-love-color p-2">
+        <section className="home d-flex align-items-center justify-content-center flex-column mx-4">
+          <img
+            src={HomeIcon}
+            alt="Home"
+            height={"18"}
+            width={"18"}
+            className="fw-bold"
+          />
+          {homePage != undefined && homePage}
         </section>
 
-        <section className="category d-flex align-items-center justify-content-center flex-column">
-          <img src={CategoryIcon} alt="Category" height={"18"} width={"18"} />
-          <CurrentPageNotch/>
+        <section className="category d-flex align-items-center justify-content-center flex-column mx-4">
+          <img
+            src={CategoryIcon}
+            alt="Category"
+            height={"18"}
+            width={"18"}
+            className="fw-bold"
+          />
+          {categoryPage != undefined && categoryPage}
         </section>
 
-        <section className="recent d-flex align-items-center justify-content-center flex-column">
-          <img src={RecentIcon} alt="Recent" height={"18"} width={"18"} />
-          <CurrentPageNotch/>
+        <section className="recent d-flex align-items-center justify-content-center flex-column mx-4">
+          <img
+            src={RecentIcon}
+            alt="Recent"
+            height={"18"}
+            width={"18"}
+            className="fw-bold"
+          />
+          {recentPage != undefined && recentPage}
         </section>
       </section>
     </React.Fragment>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CurrentPageNotch from '../components/app-current-page-notch';
 import Footer from "../components/app-footer";
 
 const Home = (): JSX.Element => {
@@ -6,7 +7,7 @@ const Home = (): JSX.Element => {
     <React.Fragment>
       <section className="container-fluid p-0">
         <h1>welcome home!</h1>
-        <Footer/>
+        <Footer homePage={<CurrentPageNotch/>} />
       </section>
     </React.Fragment>
   );
