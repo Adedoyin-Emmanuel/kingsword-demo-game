965f1de0df125e087398cb490d3e1a835df59295
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-17 02:05:02 +0100

    Added the footer-nav icons

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import CategoryIcon from "./../assets/images/category.svg";
+import HomeIcon from "./../assets/images/home.svg";
+import RecentIcon from "./../assets/images/recent.svg";
 
 const Footer = ():JSX.Element => {
     return (
