350162841f1bd87ee59e39529c403f4db6be84ad
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-17 01:00:55 +0100

    Added the autoload file and the spinner css file

---
+++ src/autoload.ts
@@ -0,0 +1,11 @@
+//import the bootstrap library
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css.map";
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/js/bootstrap.min.js.map";
+
+
+//custom styles and spinner animation file
+import "./includes/styles/style.css";
+import "./includes/scripts/script.ts";
+import "./includes/styles/three-dots.css";
