a7cdfcc3f3ce75cdcc2a7c5550c275bf670a0555
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-17 03:23:00 +0100

    Added the main-app assets

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import CurrentPageNotch from '../components/app-current-page-notch';
 import Footer from "../components/app-footer";
-
+import Swal from "sweetalert2";
 const Home = (): JSX.Element => {
   return (
     <React.Fragment>
       <section className="container-fluid p-0">
-        <h1>welcome home!</h1>
+        <h3 className="fs-3 my-3 text-capitalize text-start px-3 fw-bold">Hi!</h3>
+        
+        <p className="text-capitalize px-3">welcome to kingsword game!</p>
         <Footer homePage={<CurrentPageNotch/>} />
       </section>
     </React.Fragment>
