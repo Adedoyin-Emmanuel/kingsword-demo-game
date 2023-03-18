b11e618b87f9abc7a3553f5c0dfe6ee65a862c7f
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-18 13:16:24 +0100

    Added more padding to the question componnent

--- src/components/question.tsx
+++ src/components/question.tsx
@@ -8,7 +8,7 @@ const Question = ({ question }: questionProps): JSX.Element => {
   return (
     <React.Fragment>
       <section className="question-container d-flex align-items-center justify-content-center my-3">
-        <h5 className="fw-bold fs-5 text-center text-capitalize">{question}</h5>
+        <h5 className="fw-bold fs-5 text-center text-capitalize p-2">{question}</h5>
       </section>
     </React.Fragment>
   );
