0686a97f8b5d1bf4df3e89c5f85b4a2f08f700b1
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-18 08:02:54 +0100

    Updated the Result Page

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import db from "../backend/db";
+import Footer from "../components/app-footer";
+import CurrentPageNotch from "../components/app-current-page-notch";
 
 const Result = (): JSX.Element => {
   return (
     <React.Fragment>
-      <section className="container-fluid">
+      <section className="container-fluid p-0">
         <h1>you failed!</h1>
+        
+        <Footer recentPage={<CurrentPageNotch/>}/>
       </section>
     </React.Fragment>
   );
